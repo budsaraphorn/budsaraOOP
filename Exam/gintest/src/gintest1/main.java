17644f1d6d9aa841293ec14fd0ab24758fa1b341
budsaraphorn <95857583+budsaraphorn@users.noreply.github.com>
2023-03-13 17:41:08 +0700

    Add files via upload

---
+++ Exam/gintest/src/gintest1/main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class TriangleArea {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the base of the triangle: ");
+        double base = input.nextDouble();
+
+        System.out.print("Enter the height of the triangle: ");
+        double height = input.nextDouble();
+
+        double area = (base * height) / 2;
+
+        System.out.println("The area of the triangle is: " + area);
+    }
+}
